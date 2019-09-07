1834580d51726a31ddb385048e36bbe20c589036
KentFujii <kent.where.the.light.is@gmail.com>
2019-09-07 15:09:42 +0900

    wip: service

--- service/service_test.go
+++ service/service_test.go
@@ -1,13 +1,17 @@
-package main
+package service
 
 import (
 	"testing"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"test/service"
+	"fmt"
 )
 
 func TestService(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Service Suite")
 }
+
+var _ = Describe("service", func() {
+	fmt.Println(11111)
+})
