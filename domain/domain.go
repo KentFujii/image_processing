879594861c7fc1b4c9dd853137015c1422a00191
KentFujii <kent.where.the.light.is@gmail.com>
2019-10-07 07:38:24 +0900

    fix: s3_domain

--- domain/domain.go
+++ domain/domain.go
@@ -1,2 +1,12 @@
 package domain
 
+type s3Infra interface {
+	Put(key string, content string, contentType string) error
+	List(prefix string) ([]string, error)
+	Get(key string) ([]byte, error)
+	Delete(key string) error
+}
+
+func NewImageDomain(i s3Infra) s3Domain {
+	return imageDomain{S3: s3Infra}
+}
