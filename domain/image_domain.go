43ca885c73f82945120652c7f7e82309945978c3
KentFujii <kent.where.the.light.is@gmail.com>
2019-11-10 22:44:33 +0900

    wip

--- domain/image_domain.go
+++ domain/image_domain.go
@@ -9,7 +9,6 @@ import (
 	"strconv"
 	"strings"
 	"math/big"
-	"fmt"
 	"image"
 	_ "image/jpeg"
 	_ "image/gif"
@@ -26,14 +25,9 @@ type imageDomain struct {
 }
 
 func (d *imageDomain) ConvertFormat(bin []byte) ([]byte, error) {
-	inputBrb := bytes.NewReader(bin)
-	_, format, _ := image.DecodeConfig(inputBrb)
-	u, _ := uuid.NewRandom()
-	inputTempFile, _ := ioutil.TempFile(os.TempDir(), u.String() + "-convertFormat-*" + "." + format)
-	inputTempFile.Write(bin)
-	defer os.Remove(inputTempFile.Name())
 	var outputBrb bytes.Buffer
-	cmd := exec.Command("convert", inputTempFile.Name(), d.ConvertTo + ":-")
+	cmd := exec.Command("convert", "-", d.ConvertTo + ":-")
+	cmd.Stdin = bytes.NewReader(bin)
 	cmd.Stdout = &outputBrb
 	if err := cmd.Run(); err != nil {
 		return nil, xerrors.Errorf("ConvertFormat error: %w", err)
@@ -42,15 +36,10 @@ func (d *imageDomain) ConvertFormat(bin []byte) ([]byte, error) {
 }
 
 func (d *imageDomain) ResizeImageToLimit(bin []byte) ([]byte, error) {
-	inputBrb := bytes.NewReader(bin)
-	_, format, _ := image.DecodeConfig(inputBrb)
-	u, _ := uuid.NewRandom()
-	inputTempFile, _ := ioutil.TempFile(os.TempDir(), u.String() + "-resizeImageToLimit-*" + "." + format)
-	inputTempFile.Write(bin)
-	defer os.Remove(inputTempFile.Name())
 	var outputBrb bytes.Buffer
 	size := strconv.Itoa(d.ResizeToLimit["height"]) + "x" + strconv.Itoa(d.ResizeToLimit["width"]) + ">"
-	cmd := exec.Command("convert", inputTempFile.Name(), "-resize", size , "-")
+	cmd := exec.Command("convert", "-", "-resize", size , "-")
+	cmd.Stdin = bytes.NewReader(bin)
 	cmd.Stdout = &outputBrb
 	if err := cmd.Run(); err != nil {
 		return nil, xerrors.Errorf("Resize error: %w", err)
@@ -59,40 +48,34 @@ func (d *imageDomain) ResizeImageToLimit(bin []byte) ([]byte, error) {
 }
 
 func (i *imageDomain) CompareImage(srcBin []byte, dstBin []byte) (bool, error) {
-	_, srcFormat, _ := image.DecodeConfig(bytes.NewReader(srcBin))
-	inputSrcU, _ := uuid.NewRandom()
-	inputSrcTempFile, _ := ioutil.TempFile(os.TempDir(), inputSrcU.String() + "-compareImage-inputSrc-*" + "." + srcFormat)
-	inputSrcTempFile.Write(srcBin)
-	var outputSrcBrb bytes.Buffer
-	srcCmd := exec.Command("convert", inputSrcTempFile.Name(), "-resize", "100x100" , "-")
-	srcCmd.Stdout = &outputSrcBrb
+	var srcBrb bytes.Buffer
+	srcCmd := exec.Command("convert", "-", "-resize", "100x100" , "-")
+	srcCmd.Stdin = bytes.NewReader(srcBin)
+	srcCmd.Stdout = &srcBrb
 	if srcCmdErr := srcCmd.Run(); srcCmdErr != nil {
 		return false, xerrors.Errorf("CompareImage error: %w", srcCmdErr)
 	}
-	outputSrcU, _ := uuid.NewRandom()
-	outputSrcTempFile, _ := ioutil.TempFile(os.TempDir(), outputSrcU.String() + "-compareImage-outputSrc-*" + "." + srcFormat)
-	outputSrcTempFile.Write(outputSrcBrb.Bytes())
-	defer os.Remove(inputSrcTempFile.Name())
-	defer os.Remove(outputSrcTempFile.Name())
+	_, srcFormat, _ := image.DecodeConfig(bytes.NewReader(srcBin))
+	srcU, _ := uuid.NewRandom()
+	srcTempFile, _ := ioutil.TempFile(os.TempDir(), srcU.String() + "-compareImage-outputSrc-*" + "." + srcFormat)
+	srcTempFile.Write(srcBrb.Bytes())
+	defer os.Remove(srcTempFile.Name())
 
-	_, dstFormat, _ := image.DecodeConfig(bytes.NewReader(dstBin))
-	inputDstU, _ := uuid.NewRandom()
-	inputDstTempFile, _ := ioutil.TempFile(os.TempDir(), inputDstU.String() + "-compareImage-inputDst-*" + "." + dstFormat)
-	inputDstTempFile.Write(dstBin)
-	var outputDstBrb bytes.Buffer
-	dstCmd := exec.Command("convert", inputDstTempFile.Name(), "-resize", "100x100" , "-")
-	dstCmd.Stdout = &outputDstBrb
+	var dstBrb bytes.Buffer
+	dstCmd := exec.Command("convert", "-", "-resize", "100x100" , "-")
+	dstCmd.Stdin = bytes.NewReader(dstBin)
+	dstCmd.Stdout = &dstBrb
 	if dstCmdErr := dstCmd.Run(); dstCmdErr != nil {
 		return false, xerrors.Errorf("CompareImage error: %w", dstCmdErr)
 	}
-	outputDstU, _ := uuid.NewRandom()
-	outputDstTempFile, _ := ioutil.TempFile(os.TempDir(), outputDstU.String() + "-compareImage-outputDst-*" + "." + dstFormat)
-	outputDstTempFile.Write(outputDstBrb.Bytes())
-	defer os.Remove(inputDstTempFile.Name())
-	defer os.Remove(outputDstTempFile.Name())
+	_, dstFormat, _ := image.DecodeConfig(bytes.NewReader(dstBin))
+	dstU, _ := uuid.NewRandom()
+	dstTempFile, _ := ioutil.TempFile(os.TempDir(), dstU.String() + "-compareImage-outputDst-*" + "." + dstFormat)
+	dstTempFile.Write(dstBrb.Bytes())
+	defer os.Remove(dstTempFile.Name())
 
 	var resultBrb bytes.Buffer
-	compositeCmd := exec.Command("composite", "-compose", "difference", outputSrcTempFile.Name(), outputDstTempFile.Name(), "-")
+	compositeCmd := exec.Command("composite", "-compose", "difference", srcTempFile.Name(), dstTempFile.Name(), "-")
 	identifyCmd := exec.Command("identify", "-format", "'%[mean]'", "-")
 	pr, pw := io.Pipe()
 	compositeCmd.Stdout = pw
@@ -104,7 +87,6 @@ func (i *imageDomain) CompareImage(srcBin []byte, dstBin []byte) (bool, error) {
 	pw.Close()
 	identifyCmd.Wait()
 	diff := strings.Trim(resultBrb.String(), `'`)
-	fmt.Println(diff)
 
 	diffF, _ := strconv.ParseFloat(diff, 64)
 	diffBigF := big.NewFloat(diffF)
